2ca2b770cd86441caddeccad445b5fe2cbe35044
muraterzioglu <its.muraterzioglu@gmail.com>
2024-04-21 16:14:51 +0300

    Task Create

--- src/endpoints/taskCreate.ts
+++ src/endpoints/taskCreate.ts
@@ -19,6 +19,12 @@ export class TaskCreate extends OpenAPIRoute {
 					},
 				},
 			},
+			"400": {
+				description: "List can't be found.",
+				schema: {
+					success: false,
+				},
+			}
 		},
 	};
 
@@ -29,15 +35,27 @@ export class TaskCreate extends OpenAPIRoute {
 		data: Record<string, any>
 	) {
 		// Retrieve the validated request body
-		const taskToCreate = data.body;
+		const taskToCreate: typeof Task = data.body;
 
 		// Implement your own object insertion here
+		const listValue = await env.MY_LIST.get(taskToCreate.list)
+		if (!listValue) {
+			return Response.json({ success: false }, { status: 400 })
+		}
+		const taskValue = await env.MY_TODO.get(`${taskToCreate.list}:${taskToCreate.slug}`)
+		if (taskValue) {
+			return Response.json({ success: false }, { status: 400 })
+		}
+		await env.MY_TODO.put(`${taskToCreate.list}:${taskToCreate.slug}`, taskToCreate.task, {
+			metadata: { slug: taskToCreate.slug, task: taskToCreate.task }
+		})
 
 		// return the new task
 		return {
 			success: true,
 			task: {
 				task: taskToCreate.task,
+				list: taskToCreate.list,
 				slug: taskToCreate.slug
 			},
 		};
